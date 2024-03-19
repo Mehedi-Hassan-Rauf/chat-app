71811d77d60f3e11244542c517a74695f17216d0
Rauf <58899039+Mehedi-Hassan-Rauf@users.noreply.github.com>
2024-03-19 17:18:32 +0600

    Update SocketContext.jsx

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -16,7 +16,7 @@ export const SocketContextProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      const socket = io("https://chat-49.netlify.app", {
+      const socket = io("https://chat-app-7nqr.onrender.com", {
         query: {
           userId: authUser._id,
         },
